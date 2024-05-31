a47490bc21820b5192511654662dd08e34f54383
Saurabh <109336554+SaurabhDayal@users.noreply.github.com>
2024-06-01 00:38:01 +0530

    changes

--- database/dbHelper/dbhelper.go
+++ database/dbHelper/dbhelper.go
@@ -6,52 +6,86 @@ import (
 	"time"
 )
 
-func FindTodoById(id int) (models.TodoResponse, error) {
-	SQL := `SELECT user_id, title, description, status, pending_at, created_at FROM todos WHERE id = $1 AND archived_at IS NULL`
-	var todo models.TodoResponse
+func FindTodoById(id int) (models.Task, error) {
+	SQL := `SELECT id, 
+       			   title, 
+       			   description, 
+       			   pending_at, 
+       			   created_at 
+			FROM tasks 
+			WHERE id = $1 
+			  AND archived_at IS NULL
+`
+	var todo models.Task
 	err := database.Todo.Get(&todo, SQL, id)
 	if err != nil {
-		return models.TodoResponse{}, err
+		return models.Task{}, err
 	}
 	return todo, nil
 }
 
-func FindAllTodos() ([]models.TodoResponse, error) {
-	SQL := `SELECT user_id, title, description, status, pending_at, created_at FROM todos WHERE archived_at IS NULL`
-	todos := make([]models.TodoResponse, 0)
-	err := database.Todo.Select(&todos, SQL)
+func FindAllTodos() ([]models.Task, error) {
+	SQL := `SELECT id, 
+       			   title, 
+       			   description, 
+       			   pending_at, 
+       			   created_at 
+			FROM tasks 
+			WHERE archived_at IS NULL
+`
+	tasks := make([]models.Task, 0)
+	err := database.Todo.Select(&tasks, SQL)
 	if err != nil {
 		return nil, err
 	}
-	return todos, nil
+	return tasks, nil
 }
 
-func CreateNewTodo(todo *models.Todo, id int) error {
-	SQL := `INSERT INTO todos (user_id, title, description, pending_at) VALUES ($1,$2,$3,$4)`
-	_, err := database.Todo.Exec(SQL, id, todo.Title, todo.Description, todo.PendingAt)
+func CreateNewTodo(todo *models.Task) error {
+	SQL := `INSERT INTO tasks (
+                   title, 
+                   description, 
+                   pending_at
+                   ) 
+			VALUES ($1,$2,$3)
+`
+	_, err := database.Todo.Exec(SQL, todo.Title, todo.Description, todo.PendingAt)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func UpdateTodoById(todo models.Todo, id int) (models.TodoResponse, error) {
-	SQL := `UPDATE todos SET title=$1, description=$2, pending_at=$3 WHERE id=$4`
+func UpdateTodoById(todo models.Task, id int) (models.Task, error) {
+	SQL := `UPDATE tasks 
+			SET title = $1, 
+			    description= $2, 
+			    pending_at= $3 
+			WHERE id = $4
+`
 	_, err := database.Todo.Exec(SQL, todo.Title, todo.Description, todo.PendingAt, id)
 	if err != nil {
-		return models.TodoResponse{}, err
+		return models.Task{}, err
 	}
-	SQL2 := `SELECT user_id, title, description, status, pending_at, created_at FROM todos WHERE id = $1 AND archived_at IS NULL`
-	var todo2 models.TodoResponse
+	SQL2 := `SELECT id, 
+       				title, 
+       				description, 
+       				pending_at, 
+       				created_at 
+			FROM tasks 
+			WHERE id = $1 
+			  AND archived_at IS NULL
+`
+	var todo2 models.Task
 	err2 := database.Todo.Get(&todo2, SQL2, id)
 	if err2 != nil {
-		return models.TodoResponse{}, err2
+		return models.Task{}, err2
 	}
 	return todo2, nil
 }
 
 func DeleteTodoById(id int) error {
-	SQL := `UPDATE todos SET archived_at =$1 WHERE id=$2 AND archived_at IS NULL`
+	SQL := `UPDATE tasks SET archived_at =$1 WHERE id=$2 AND archived_at IS NULL`
 	_, err := database.Todo.Exec(SQL, time.Now(), id)
 	if err != nil {
 		return err
--- models/model.go
+++ models/model.go
@@ -1,18 +1,15 @@
 package models
 
-import "time"
+import (
+	"github.com/volatiletech/null"
+	"time"
+)
 
-type Todo struct {
+type Task struct {
+	ID          string    `json:"id" db:"id"`
 	Title       string    `json:"title" db:"title"`
 	Description string    `json:"description" db:"description"`
-	PendingAt   time.Time `json:"pendingAt" db:"pending_at"`
-}
-
-type TodoResponse struct {
-	UserId      int       `json:"userId" db:"user_id"`
-	Title       string    `json:"title" db:"title"`
-	Description string    `json:"description" db:"description"`
-	Status      bool      `json:"status" db:"status"`
 	PendingAt   time.Time `json:"pendingAt" db:"pending_at"`
 	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
+	ArchivedAt  null.Time `json:"archivedAt" db:"archived_at"`
 }
